68721e8eecca85070bddd19ea6c418ddaea5630a
Thom May <thom@chef.io>
2017-07-28 15:35:38 +0100

    general tidiness
    Signed-off-by: Thom May <thom@chef.io>

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,5 @@
 require "bundler/setup"
-require "chef/telemetry"
+require "telemetry"
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
--- spec/telemetry_spec.rb
+++ spec/telemetry_spec.rb
@@ -1,11 +1,8 @@
 require "spec_helper"
 
-RSpec.describe Chef::Telemetry do
+RSpec.describe Telemetry do
   it "has a version number" do
-    expect(Chef::Telemetry::VERSION).not_to be nil
+    expect(Telemetry::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
